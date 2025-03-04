b8055f882051cc7ebfde04affea94ab0330c20f9
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-04 22:27:44 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -14,3 +14,17 @@ class Student {
         setcgpa(cgpa);
     }
 
+    
+    public String getName() {
+        return name;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPRN() {
+        return prn;
+    }
+
