463a0467cdc1e8040e8b2077bc7ca30bbf79a371
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-04 22:28:57 +0530

    Update StudentOperation.java

--- StudentOperation.java
+++ StudentOperation.java
@@ -13,3 +13,25 @@ class StudentOperations {
         System.out.println("Student Added");
     }
 
+    
+    public void displayStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students to display.");
+            return;
+        }
+        for (Student student : students) {
+            student.display();
+        }
+    }
+
+    public Student searchStudentByPRN(int prn) {
+        for (Student student : students) {
+            if (student.getPRN() == prn) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    
+
