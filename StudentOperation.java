8314c39fb849674244ecf2af1cc2a710884eadae
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-03-04 22:37:08 +0530

    Update StudentOperation.java

--- StudentOperation.java
+++ StudentOperation.java
@@ -71,6 +71,23 @@ class StudentOperations {
     }
 
     
+    public boolean deleteStudent(int prn) {
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()) {
+            Student student = iterator.next();
+            if (student.getPRN() == prn) {
+                iterator.remove();
+                System.out.println("Student removed successfully.");
+                return true;
+            }
+        }
+        System.out.println("Student with PRN " + prn + " not found.");
+        return false;
+    }
+}
+
+
+    
 
     
 
