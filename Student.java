9fad66864c4cb5d54d6f97723666a79a16b9f237
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-04-18 21:56:50 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,72 +1,23 @@
-class Student {
-    private String name;
+public class Student {
+    private String name, branch, batch;
     private int prn;
-    private String branch;
-    private String batch;
     private float cgpa;
 
-    
-    public Student(String name, int prn, String branch, String batch, float CGPA) {
-        setName(name);
-        setPRN(prn);
-        setBranch(branch);
-        setBatch(batch);
-        setcgpa(cgpa);
-    }
-
-    
-    public String getName() {
-        return name;
-
-    }
-
-    public void setName(String name) {
+    public Student(String name, int prn, String branch, String batch, float cgpa) {
         this.name = name;
-    }
-
-    public int getPRN() {
-        return prn;
-    }
-
-    
-    public void setPRN(int prn) {
         this.prn = prn;
-    }
-
-    public String getBranch() {
-        return branch;
-
-    }
-
-    public void setBranch(String branch) {
         this.branch = branch;
-    }
-
-    
-    public String getBatch() {
-        return batch;
-
-    }
-
-    public void setBatch(String batch) {
         this.batch = batch;
-    }
-
-    public float getcgpa() {
-        return cgpa;
-
-    }
-
-    public void setcgpa(float cgpa) {
         this.cgpa = cgpa;
     }
 
-    
+    // Getters and setters...
+
     public void display() {
-        System.out.println("Name: " + getName() + " PRN: " + getPRN() + "Branch: " + getBranch() + "Batch: "
-                + getBatch() + "CGPA: " + getcgpa());
+        System.out.println("Name: " + name);
+        System.out.println("PRN: " + prn);
+        System.out.println("Branch: " + branch);
+        System.out.println("Batch: " + batch);
+        System.out.println("CGPA: " + cgpa);
     }
 }
-
-    
-
